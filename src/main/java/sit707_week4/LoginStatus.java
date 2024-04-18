0af387e10896044d5703cde0fdb23711489fa228
Ishini Bhagya <59154447+ishini98@users.noreply.github.com>
2024-04-18 16:16:08 +0530

    Add files via upload

---
+++ src/main/java/sit707_week4/LoginStatus.java
@@ -0,0 +1,28 @@
+package sit707_week4;
+
+
+public class LoginStatus {
+
+	private boolean loginSuccess = false;
+	private String errorMsg = "";
+	
+	public LoginStatus(boolean status, String errorMsg) {
+		this.loginSuccess = status;
+		this.errorMsg = errorMsg;
+	}
+
+	public boolean isLoginSuccess() {
+		return loginSuccess;
+	}
+
+	public String getErrorMessage() {
+		return errorMsg;
+	}
+
+	@Override
+	public String toString() {
+		return "LoginStatus [loginSuccess=" + loginSuccess + ", errorMsg=" + errorMsg + "]";
+	}
+	
+	
+}
